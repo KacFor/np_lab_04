668a39e00ff70a836a1d8edae464c2e4229e5590
KacFor <154218724+KacFor@users.noreply.github.com>
2024-01-22 23:44:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from app import add_numbers, subtract_numbers, multiply_numbers, divide_numbers
 
+
 class TestMathFunctions(unittest.TestCase):
 
     def test_add_numbers(self):
@@ -22,9 +23,9 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(divide_numbers(6, 3), 2.0)
         self.assertEqual(divide_numbers(5, 2), 2.5)
         self.assertEqual(divide_numbers(0, 5), 0.0)
-
         with self.assertRaises(ValueError):
             divide_numbers(5, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
